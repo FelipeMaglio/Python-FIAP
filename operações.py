1761d14577ef9436da0c1dde1f52e7223e6864eb
Felipe Maglio <felipemagliofilho@gmail.com>
2025-03-17 09:38:38 -0300

    Add files via upload

---
+++ operações.py
@@ -0,0 +1,23 @@
+A = 11
+B = 3
+print(A+B*A)
+print(B*A+B)
+print(A//B)
+print(B%A)
+print(A%B)
+print(B//A)
+print(B-A/B)
+print(A/B-A*B)
+print(10+A%B)
+print(10-B//A)
+print(B//A-50)
+print(A+B%A-A//B*2)
+print(A*(B+A)-(A-B)/2)
+
+X=True 
+Y=False
+print (X, Y)
+print (X or Y)
+print(X,Y or X)
+print(X or Y or X)
+print()
